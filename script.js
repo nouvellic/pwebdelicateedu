81fec022c35705e74d68a2a07bdf96228ad11052
nouvellic <55073331+nouvellic@users.noreply.github.com>
2023-09-24 08:35:48 +0700

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Handle login logic here
+    alert('Login button clicked');
+});
+
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Handle signup logic here
+    alert('Sign Up button clicked');
+});
